3dc4ebb97c7196b7849d93df51345346996fb5f2
MCausc78 <101461652+MCausc78@users.noreply.github.com>
2022-05-11 22:35:16 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 VERSION="1.0"
-rm -rf ./build
+if [ -e build ]; then
+    rm -rf ./build
+    mkdir build
+    echo "Обнаружен старый билд!"
+    echo "Мы удалим его, и скомплируем новый билд"
+else
+    # continue
+fi
 x86_64-w64-mingw32-g++ -o ./build/calc-win64-${VERSION}.exe
 i686-w64-mingw32-g++ -o ./build/calc-win32-${VERSION}.exe ./src/calc.cpp
 g++ -o ./build/calc-unix-${VERSION} ./src/calc.cpp
