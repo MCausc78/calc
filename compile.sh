42715e4652dff07274718629f638e2487138d07e
MCausc78 <101461652+MCausc78@users.noreply.github.com>
2022-05-11 22:33:36 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-x86_64-w64-mingw32-g++ -o calc-win64.exe
-i686-w64-mingw32-g++ -o calc-win32.exe ./src/calc.cpp
-g++ -o calc-unix ./src/calc.cpp
+VERSION="1.0"
+rm -rf ./build
+x86_64-w64-mingw32-g++ -o ./build/calc-win64-${VERSION}.exe
+i686-w64-mingw32-g++ -o ./build/calc-win32-${VERSION}.exe ./src/calc.cpp
+g++ -o ./build/calc-unix-${VERSION} ./src/calc.cpp
 echo "Скомплировал (или нет)"
