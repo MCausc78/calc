a9d54d7d5300be359114f18981570e3cb3dbd33e
Говнич <101461652+MCausc78@users.noreply.github.com>
2022-05-19 12:22:52 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/usr/bin/bash
+
 VERSION="1.0"
 if [ -e build ]; then
     rm -rf ./build
     mkdir build
     echo "Обнаружен старый билд!"
     echo "Мы удалим его, и скомплируем новый билд"
-else
-    # continue
 fi
-if [ $OS == "Windows_NT" ]; then
+if [[ $OS == "Windows_NT" ]]; then
     echo "У вас обнаружен Windows. Мы не можем скомплировать программу на вашей ОС!"
     exit 1
 else
@@ -19,10 +18,12 @@ else
             sudo apt install gcc gcc-mingw-w64-x86-64 gcc-mingw-w64-i686
             ;;
         n|no|not|N|No|NO|nO|н|нет|Н|Нет|НЕт|НЕТ|нЕТ|неТ|неа|неА|нЕА|НЕА|Неа|НЕа|ноу|ноУ|нОУ|НОУ|НОу|Ноу)
-            
             ;;
     esac
-x86_64-w64-mingw32-g++ -o ./build/calc-win64-${VERSION}.exe
+fi
+x86_64-w64-mingw32-g++ -o ./build/calc-win64-${VERSION}.exe ./src/calc.cpp
 i686-w64-mingw32-g++ -o ./build/calc-win32-${VERSION}.exe ./src/calc.cpp
 g++ -o ./build/calc-unix-${VERSION} ./src/calc.cpp
+g++ -s -o ./build/calc-asm-${VERSION}.asm ./src/calc.cpp
+chmod a+x ./build/*
 echo "Скомплировал (или нет)"
